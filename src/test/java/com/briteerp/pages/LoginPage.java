f65e3a0f2352f43f9c104046c5f54638c2e7cf1b
MustafaYagci <62900273+MustafaYagci@users.noreply.github.com>
2020-06-16 12:44:54 +0200

    Update LoginPage.java

--- src/test/java/com/briteerp/pages/LoginPage.java
+++ src/test/java/com/briteerp/pages/LoginPage.java
@@ -1,4 +1,21 @@
 package com.briteerp.pages;
 
+import com.briteerp.utilities.ConfigurationReader;
+import com.briteerp.utilities.Driver;
+import org.openqa.selenium.By;
+
 public class LoginPage {
+
+    public static void Login(){
+        
+        Driver.get().get(ConfigurationReader.get("url"));
+        Driver.get().findElement(By.id("login")).sendKeys(ConfigurationReader.get("username"));
+        Driver.get().findElement(By.id("password")).sendKeys(ConfigurationReader.get("password"));
+        
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
